b3aca08acd268f6f1002aad662e1ac8076318666
dhanushequinox <dhanush@equinoxtechsolutions.com>
2025-05-16 15:48:44 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/ci_cd_counter_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ci_cd_counter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CI/CD Demo',
+      home: const CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _count = 0;
+
+  void _increment() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("CI/CD Counter App")),
+      body: Center(child: Text('Count: $_count', style: const TextStyle(fontSize: 32))),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _increment,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
